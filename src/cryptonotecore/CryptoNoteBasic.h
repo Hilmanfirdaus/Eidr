0fa02367f6494a9d2d61df1c584e1a2b1e5950d0
NinjaCoin-Master <ninjacoinmaster@gmail.com>
2019-08-24 14:05:23 +0200

    update to Argon2id algo

---
+++ src/cryptonotecore/CryptoNoteBasic.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2019, The NinjaCoin Developers
+//
+// Please see the included LICENSE file for more information.
+
+#pragma once
+
+#include <CryptoNote.h>
+#include <boost/utility/value_init.hpp>
+
+namespace CryptoNote
+{
+    KeyPair generateKeyPair();
+}
---
+++ src/zedwallet++/Sync.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2019, The NinjaCoin Developers
+//
+// Please see the included LICENSE file for more information.
+
+#pragma once
+
+#include <walletbackend/WalletBackend.h>
+
+void syncWallet(const std::shared_ptr<WalletBackend> walletBackend);
