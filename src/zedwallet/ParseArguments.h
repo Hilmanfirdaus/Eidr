4e9425911e36a044d7d7832a0bd09039f3e75e14
NinjaCoin-Master <ninjacoinmaster@gmail.com>
2019-08-24 14:04:16 +0200

    Update

--- src/zedwallet/ParseArguments.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2019, The NinjaCoin Developers
-//
-// Please see the included LICENSE file for more information.
-
-#pragma once
-
-#include <zedwallet/Types.h>
-
-Config parseArguments(int argc, char **argv);
