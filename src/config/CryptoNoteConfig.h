06e2c11d954c28e9615a5cb1a2f89a11164d3c7b
NinjaCoin <ninjacoinmaster@gmail.com>
2020-02-27 14:02:34 +0100

    Update to v2.0.0

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -124,7 +124,7 @@ namespace CryptoNote
 
         const uint32_t MIXIN_LIMITS_V3_HEIGHT = 472000;
 
-        /* The mixin to use by default with zedwallet and turtle-service */
+        /* The mixin to use by default with zedwallet and ninja-service */
         /* DEFAULT_MIXIN_V0 is the mixin used before MIXIN_LIMITS_V1_HEIGHT is started */
         const uint64_t DEFAULT_MIXIN_V0 = 3;
 
